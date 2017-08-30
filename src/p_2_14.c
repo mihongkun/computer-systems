c0f33353b6674967a6e00015533e236a5a3a8898
mihk <mihongkun@gmail.com>
2017-08-30 09:01:22 +0800

    add something

--- src/p_2_14.c
+++ src/p_2_14.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main(){
+	int x = 0x66,y=0x39;
+	printf("x & y = %x,\tx && y = %x\n",x & y, x && y);
+	printf("x | y = %x,\tx || y = %x\n",x | y, x || y);
+	printf("~x | ~y = %x,\t~x || ~y = %x\n",~x | ~y, ~x || ~y);
+	printf("~x & ~y = %x,\t~x && ~y = %x\n",~x & ~y, ~x && ~y);
+	return 0;
+}
